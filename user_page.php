0acf4b24e0ef78e3ba16d2d75c43212a586db52f
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-02-10 19:44:57 +0530

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h3>Hi, <span>User</span></h3>
+            <h1>Welcome <span></span></h1>
+            <p>This is a user page</p>
+            <a href="login_form.php" class="btn">Login</a>
+            <a href="register_form.php" class="btn">Register</a>
+            <a href="logout.php" class="btn">Logout</a>
+        </div>
+    </div>
+</body>
+</html>
