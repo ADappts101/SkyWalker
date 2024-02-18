1c4215f0bc51540cab8bd6c414cd0db318ce31ff
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-02-18 20:39:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,6 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="container">
-        <div class="content">
-            <h3>Hi, <span>User</span></h3>
-            <h1>Welcome <span></span></h1>
-            <p>This is a user page</p>
-            <a href="login_form.php" class="btn">Login</a>
-            <a href="register_form.php" class="btn">Register</a>
-            <a href="logout.php" class="btn">Logout</a>
-        </div>
-    </div>
+    
 </body>
 </html>
