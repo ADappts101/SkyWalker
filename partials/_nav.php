f7d97f5682561e5b07deac761be43d24e878e6ff
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-02-18 21:04:16 +0530

    Update _nav.php

--- partials/_nav.php
+++ partials/_nav.php
@@ -1,3 +1,4 @@
+<?php header('Content-Type: text/html; charset=utf-8');?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
